99c93c7d5009ef447dfd09911146adbf30c75c1e
v.kolesnikov <v.kolesnikov@digitalcity.group>
2020-10-09 05:33:19 +0300

    mobile menu

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  document.getElementsByClassName('open')[0].onclick = () => {
+    document.getElementsByClassName('mobile-menu-content')[0].classList.add('opened');
+    document.getElementsByTagName('body')[0].classList.add('stop-scroll');
+  };
+  document.getElementsByClassName('close')[0].onclick = () => {
+    document.getElementsByClassName('mobile-menu-content')[0].classList.remove('opened');
+    document.getElementsByTagName('body')[0].classList.remove('stop-scroll');
+  }
+};
